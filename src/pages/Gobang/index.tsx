d95e9bc32eb709b5c97b3f14ec94bbf7dbe5b8a2
林大人 <lindada0809@gmail.com>
2021-07-09 16:53:47 +0800

    feat: add move history

--- src/pages/Gobang/index.tsx
+++ src/pages/Gobang/index.tsx
@@ -11,27 +11,51 @@ function Gobang() {
     },
   ]);
   const [xIsNext, setXIsNext] = useState(true);
-  // 当前棋盘
-  const current = history[history.length - 1];
-  const winner = calculateWinner(current.squares);
-  let status: {} | null | undefined;
-
-  if (winner) {
-    status = `Winner: ${winner}`;
-  } else {
-    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-  }
+  const [stepNumber, setStepNumber] = useState(0);
 
   const handleClick = (i: string | number) => {
+    const curHistory = history.slice(0, stepNumber + 1);
+    const current = curHistory[curHistory.length - 1];
     const squares = [...current.squares];
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = xIsNext ? 'X' : 'O';
-    const historyTemp = [...history, { squares }];
+    const historyTemp = [...curHistory, { squares }];
     setHistory(historyTemp);
     setXIsNext(!xIsNext);
+    setStepNumber(curHistory.length);
   };
+
+  // 历史跳转
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  // 历史记录
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : 'Go to game start';
+    const key = `move${move}`;
+    return (
+      <li key={key}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  // 当前棋盘
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+  let status: {} | null | undefined;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (moves.length === 10) {
+    status = 'Draw, start over game';
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
   return (
     <View>
@@ -41,7 +65,7 @@ function Gobang() {
         </div>
         <div className={styles.gameInfo}>
           <div className={styles.status}>{status}</div>
-          {/* <ol>{moves}</ol> */}
+          <ol>{moves}</ol>
         </div>
       </div>
     </View>
