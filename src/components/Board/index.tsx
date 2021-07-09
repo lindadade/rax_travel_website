6d108aff95d8595bbe0d01fed11b4498d3bca938
林大人 <lindada0809@gmail.com>
2021-07-09 16:30:23 +0800

    feat: add history

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,52 +1,29 @@
-import { createElement, useState } from 'rax';
+import { createElement } from 'rax';
 import View from 'rax-view';
 import Square from '../Square';
 import styles from './index.module.css';
-import { calculateWinner } from '@/utils';
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const handleClick = (i) => {
-    const square = [...squares];
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    square[i] = xIsNext ? 'X' : 'O';
-    setSquares(square);
-    setXIsNext(!xIsNext);
+function Board(props: { squares: { [x: string]: any; }; onClick: (arg0: any) => any; }) {
+  const renderSquare = (i: number) => {
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
   };
-
-  const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
-  };
-
-  const winner = calculateWinner(squares);
-  let status: {} | null | undefined;
-  if (winner) {
-    status = `Winner: ${winner}`;
-  } else {
-    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-  }
 
   return (
     <View>
-      <div className={styles.status}>{status}</div>
       <div className={styles.boardRow}>
+        {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-        {renderSquare(3)}
       </div>
       <div className={styles.boardRow}>
+        {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-        {renderSquare(6)}
       </div>
       <div className={styles.boardRow}>
+        {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-        {renderSquare(9)}
       </div>
     </View>
   );
--- src/pages/Gobang/index.tsx
+++ src/pages/Gobang/index.tsx
@@ -1,17 +1,46 @@
-import { createElement } from 'rax';
+import { createElement, useState } from 'rax';
 import View from 'rax-view';
 import Board from '@/components/Board';
 import styles from './index.module.css';
+import { calculateWinner } from '@/utils';
 
 function Gobang() {
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+    },
+  ]);
+  const [xIsNext, setXIsNext] = useState(true);
+  // 当前棋盘
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+  let status: {} | null | undefined;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const handleClick = (i: string | number) => {
+    const squares = [...current.squares];
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? 'X' : 'O';
+    const historyTemp = [...history, { squares }];
+    setHistory(historyTemp);
+    setXIsNext(!xIsNext);
+  };
+
   return (
     <View>
       <div className={styles.game}>
         <div className={styles.gameBoard}>
-          <Board />
+          <Board squares={current.squares} onClick={(i) => handleClick(i)} />
         </div>
         <div className={styles.gameInfo}>
-          <div>{status}</div>
+          <div className={styles.status}>{status}</div>
           {/* <ol>{moves}</ol> */}
         </div>
       </div>
