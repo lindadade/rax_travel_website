d0a81a8ec5d083879b2506c922026f97629631db
林大人 <lindada0809@gmail.com>
2021-07-11 13:17:58 +0800

    feat: Board format map() render Square

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -3,28 +3,22 @@ import View from 'rax-view';
 import Square from '../Square';
 import styles from './index.module.css';
 
-function Board(props: { squares: { [x: string]: any; }; onClick: (arg0: any) => any; }) {
+function Board(props: { squares: { [x: string]: any }; onClick: (arg0: any) => any }) {
   const renderSquare = (i: number) => {
-    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+    return <Square key={i} value={props.squares[i]} onClick={() => props.onClick(i)} />;
   };
 
   return (
     <View>
-      <div className={styles.boardRow}>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className={styles.boardRow}>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className={styles.boardRow}>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {[0, 1, 2].map((v) => {
+        return (
+          <div key={v} className={styles.boardRow}>
+            {[NaN, NaN, NaN].map((v2, i) => {
+              return (renderSquare(v * 3 + i));
+            })};
+          </div>
+        );
+      })}
     </View>
   );
 }
